24a132d1f94ad3a34999436acfddfbe4a3d4fde5
marcelinho21junior <marcelo.belitardo@fatec.sp.gov.br>
2022-04-02 17:53:02 -0300

    commits ends

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ function openNav(){
 
     let btn = document.querySelector('.nav-button')
     btn.style.backgroundColor='white'
+
+    let msg = document.querySelector('.msgWelcome')
+    msg.textContent=''
 }
 
 function closeNav(){
@@ -11,4 +14,8 @@ function closeNav(){
     
     let btn = document.querySelector('.nav-button')
     btn.style.backgroundColor=''
+
+    let msg = document.querySelector('.msgWelcome')
+    msg.innerHTML = `<h1>SEJA BEM VINDO(A) NOVAMENTE</h1>`
 }
+
